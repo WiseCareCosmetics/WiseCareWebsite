9b8e43440b9226796d7352fce4a0843e966d4a25
AbdelrahmanAhmed04 <89594421+AbdelrahmanAhmed04@users.noreply.github.com>
2022-08-27 05:36:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).on("scroll", function () {
+  if ($(document).scrollTop() > 80) {
+    $(".nav-content").addClass("shrink");
+    $("nav").addClass("shrink");
+  } else {
+    $(".nav-content").removeClass("shrink");
+    $("nav").removeClass("shrink");
+  }
+});
+var typed = new Typed(".auto-type", {
+  strings: ["Appropriate price.", "High quality.", "Safe & natural products."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "abdelrahman.ahmed.mail@gmail.com",
+    Password: "Body#55647#body",
+    To: "abdo.gaming666@gmail.com",
+    From: document.getElementById("emailInfo").value,
+    Subject: "new contact form email",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
